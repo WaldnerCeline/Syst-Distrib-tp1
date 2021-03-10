756985b1772a9fe0f38bbbfc683d98b9ed86b5f6
WaldnerCeline <50487569+WaldnerCeline@users.noreply.github.com>
2021-03-10 16:37:17 +0100

    Add files via upload

---
+++ Tuto/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpanningTree tree = new SpanningTree();
+		String message =tree.getDescription();
+		tree.beforeStart();
+		tree.onStarCenter();
+		System.out.println(tree);
+	}
+
+}
